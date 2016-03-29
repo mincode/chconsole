615f2be18f83b0a48e11d8685894b3272f6d8054
Manfred Minimair <manfred@minimair.org>
2016-03-29 19:47:20 -0400

    working on config files

---
+++ chconsole/configure/configuration.py
@@ -0,0 +1,15 @@
+import json
+
+__author__ = 'Manfred Minimair <manfred@minimair.org>'
+
+# non-user editable configuration files
+
+# with open('config.json') as json_data_file:
+#     data = json.load(json_data_file)
+#
+# with open('config.json', 'w') as outfile:
+#     json.dump(data, outfile)
+
+# write default configuration file
+# write on change
+# load on access
